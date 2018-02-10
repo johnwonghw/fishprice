dda4ff2ab831e02fd03fe9708dfff62af67c1123
Josh Coles <josh@joshcoles.com>
2018-02-10 14:31:55 -0800

    Set up onboard container.

---
+++ client/src/containers/Onboard/Onboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Onboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Onboard</h1>
+      </div>
+    )
+  }
+}
+
+export default Onboard;
