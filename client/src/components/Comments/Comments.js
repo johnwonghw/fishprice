e9f62cb178f66795d23be2fbdf9e78f89f33be08
Josh Coles <josh@joshcoles.com>
2018-02-11 12:53:16 -0800

    Continued styling pages.

--- client/src/components/Comments/Comments.js
+++ client/src/components/Comments/Comments.js
@@ -6,31 +6,31 @@ class Comments extends Component {
 
    comments = [
      {
-       name: 'Vinny',
-       location: 'Hope, BC',
+       name: 'Jeff Ross',
+       location: 'Norton Sound, BC',
        phoneNumber: '(604) 555-555',
-       comment: "Heading up to whistler rent west coast best coast sedins granville lions commercial vancouverites skytrain main street stanley park government developers stanley park stanley park heading up to whistler cambie vancouverites translink ramen main street arby's stanley park heading up to whistler vancouverites ramen vancouverites expensive west coast best coast new brunch spot on main street broadway I just need to be close to the mountains.",
+       comment: "On Friday I sold about 200 lbs worth of Sockeye salmon to Richard's Eatery in Nome, Alaska. They gave me a decent price and I would do business with them again.",
+       soldPrice: 0.75,
+       buyer: "Richard's Eatery",
+       species: 'Sockeye'
+     },
+     {
+       name: 'Jennifer Hope',
+       location: 'Kodiak',
+       phoneNumber: '(604) 555-555',
+       comment: "Sold 50 lbs of chum on Wednesday to the hotel. Guy tried to offer me 27 cents per pound and I laughed in his face. They ended up giving me 35 cents a pound but I won't be doing business with them again any time soon.",
        soldPrice: 4.3,
        buyer: 'MEC',
        species: 'Cod'
      },
      {
-       name: 'Joe',
-       location: 'Vancouver',
+       name: 'Tim Treadwell',
+       location: 'Alaska Peninsula',
        phoneNumber: '(604) 555-555',
-       comment: "Heading up to whistler rent west coast best coast sedins granville lions commercial vancouverites skytrain main street stanley park government developers stanley park stanley park heading up to whistler cambie vancouverites translink ramen main street arby's stanley park heading up to whistler.",
-       soldPrice: 4.3,
-       buyer: 'MEC',
-       species: 'Cod'
-     },
-     {
-       name: 'Joe',
-       location: 'Vancouver',
-       phoneNumber: '(604) 555-555',
-       comment: "Heading up to whistler rent west coast best coast sedins granville lions commercial vancouverites skytrain main street stanley park government developers stanley park stanley park heading up to whistler cambie vancouverites translink ramen main street arby's stanley park heading up to whistler vancouverites ramen vancouverites expensive west coast best coast new brunch spot on main street broadway I just need to be close to the mountains.",
-       soldPrice: 4.3,
-       buyer: 'MEC',
-       species: 'Cod'
+       comment: "Caught a bunch of Coho the other day but couldn't get anything above $0.25 a pound so I'm freezing it and eating it myself lol.",
+       soldPrice: 0.25,
+       buyer: 'My Belly',
+       species: 'Coho'
      }
    ]
 
