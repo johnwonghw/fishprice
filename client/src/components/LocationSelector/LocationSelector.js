7bda1083bd8219d7b5a264449911b068f7a4fe16
Josh Coles <josh@joshcoles.com>
2018-02-10 21:16:49 -0800

    Fixed styling issues on button navigation.

--- client/src/components/LocationSelector/LocationSelector.js
+++ client/src/components/LocationSelector/LocationSelector.js
@@ -11,24 +11,27 @@ class SpeciesSelector extends Component {
       })
 
     return (
-      <div className="container onboard">
-        <div className="row">
-          <div className="col-xs-12 col-sm-8 col-sm-offset-2 select-container">
-            <div className="form-group">
-              <form onSubmit={this.props.onSubmitLocation}>
-                <h2>Where are you fishing today?</h2>
-                <select onChange={this.props.onChangeLocation} className="form-control">
-                  {locations}
-                </select>
-                <div className="button-navigation">
-                  <button className="btn btn-default" onClick={this.props.onReturnToSpecies}>Go Back</button>
-                  <input type="submit" value="next" className="form-control btn btn-success"></input>
-                </div>
-              </form>
+      <form onSubmit={this.props.onSubmitLocation}>
+        <div className="container onboard">
+          <div className="button-navigation">
+            <button onClick={this.props.onReturnToSpecies}>Go Back</button>
+            <input type="submit" value="next"></input>
+          </div>
+          <div className="row">
+            <div className="col-xs-12 col-sm-8 col-sm-offset-2 select-container">
+              <div className="form-group">
+                
+                  <h2>Where are you fishing today?</h2>
+                  <select onChange={this.props.onChangeLocation} className="form-control">
+                    {locations}
+                  </select>
+                  
+                
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </form>
     )
   }
 }
--- client/src/components/SpeciesSelector/SpeciesSelector.js
+++ client/src/components/SpeciesSelector/SpeciesSelector.js
@@ -11,24 +11,24 @@ class SpeciesSelector extends Component {
     })
 
     return (
+      <form onSubmit={this.props.onSubmitSpecies}>
       <div className="container onboard">
         <div className="button-navigation">
-          <div onClick={this.props.onReturnHome}><i class="fas fa-chevron-left"></i></div>
-          <input type="submit" value="Next"></input>
+          <div onClick={this.props.onReturnHome}><i className="fas fa-chevron-left"></i></div>
+          <input type="submit" value="NEXT"></input>
         </div>
         <div className="row">
           <div className="col-xs-12 col-sm-8 col-sm-offset-2 select-container">
-            <form onSubmit={this.props.onSubmitSpecies}>
-              <div className="form-group">
-                <h2>What are you fishing for today?</h2>
-                  <select onChange={this.props.onChangeSpecies} className="form-control">
-                    {species}
-                  </select>
-                </div>
-            </form>
+            <div className="form-group">
+              <h2>What are you fishing for today?</h2>
+                <select onChange={this.props.onChangeSpecies} className="form-control">
+                  {species}
+                </select>
+              </div>
           </div>
         </div>
       </div>
+      </form>
     )
   }
 }
